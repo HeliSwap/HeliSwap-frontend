4e20a3d31b9cd11a353fa0acf1ea3bcda3698638
Martin Dobrev <martindobrev87@gmail.com>
2022-04-19 11:25:08 +0300

    Warnings removed

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -6,25 +6,25 @@ const Header = () => {
       <div className="container">
         <div className="d-flex justify-content-between">
           <div>
-            <a className="link mx-4" href="#">
+            <a className="link mx-4" href="#index">
               USD
             </a>
-            <a className="link mx-4" href="#">
+            <a className="link mx-4" href="#index">
               English
             </a>
           </div>
 
           <div>
-            <a className="link mx-4" href="#">
+            <a className="link mx-4" href="#index">
               Support
             </a>
-            <a className="link mx-4" href="#">
+            <a className="link mx-4" href="#index">
               FAQ
             </a>
-            <a className="link mx-4" href="#">
+            <a className="link mx-4" href="#index">
               Privacy policy
             </a>
-            <a className="link mx-4" href="#">
+            <a className="link mx-4" href="#index">
               Terms of service
             </a>
           </div>
