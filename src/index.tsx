303d462b60089f8457a42dab6896fc489af63764
Martin Dobrev <martindobrev87@gmail.com>
2022-04-19 10:04:56 +0300

    Custom bootstrap styling added

--- src/index.tsx
+++ src/index.tsx
@@ -2,6 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './components/App';
 
+import './styles/styles.scss';
+
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
 
 root.render(
