99ec9114bf12a0ec533b6f590a5ac974ae65dad5
Martin Dobrev <martindobrev87@gmail.com>
2022-04-20 08:43:17 +0300

    Connection with Hashpack added
    Connection updated WIP
    
    
    Connection updated
    
    
    Connection updated
    
    
    Disconnect function added
    
    
    Connection updated

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,5 +1,6 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import { HashConnect, HashConnectTypes } from 'hashconnect';
 
 import Home from '../pages/Home';
 import Styleguide from '../pages/Styleguide';
@@ -8,10 +9,103 @@ import Header from './Header';
 import Footer from './Footer';
 
 function App() {
+  const [connected, setConnected] = useState(false);
+  const [isConnectionLoading, setIsConnectionLoading] = useState(true);
+  const [readyToConnect, setReadyToConnect] = useState(false);
+  const [hashconnectInstance, setHashconnectInstance] = useState<HashConnect>();
+  const [walletMetadata, setWalletMetadata] = useState({});
+  const [connectionData, setConnectionData] = useState({
+    topic: '',
+    pairingString: '',
+    privateKey: '',
+    pairedWalletData: undefined,
+    pairedAccounts: [],
+  });
+
+  const initHashConnect = async () => {
+    const hashconnect = new HashConnect();
+
+    setHashconnectInstance(hashconnect);
+
+    const appMetadata: HashConnectTypes.AppMetadata = {
+      name: 'HeliSwap DEX',
+      description: 'HeliSwap DEX',
+      icon: 'https://absolute.url/to/icon.png',
+    };
+
+    // Check for already saved login data
+    const foundData = localStorage.getItem('hashconnectData');
+
+    if (foundData) {
+      const savedData = JSON.parse(foundData);
+      setReadyToConnect(true);
+      await hashconnect.init(appMetadata, savedData.privateKey);
+      await hashconnect.connect(savedData.topic, savedData.pairedWalletData);
+      setConnected(true);
+    } else {
+      const initData = await hashconnect.init(appMetadata);
+      const privateKey = initData.privKey;
+      const state = await hashconnect.connect();
+      const topic = state.topic;
+      const pairingString = hashconnect.generatePairingString(state, 'testnet', false);
+
+      setConnectionData(prev => ({ ...prev, privateKey, topic, pairingString }));
+
+      hashconnect.findLocalWallets();
+    }
+  };
+
+  const connectWallet = () => {
+    if (walletMetadata && hashconnectInstance) {
+      setIsConnectionLoading(true);
+      hashconnectInstance.connectToLocalWallet(connectionData.pairingString);
+    }
+  };
+
+  const disconnectWallet = () => {
+    localStorage.removeItem('hashconnectData');
+    setConnected(false);
+    setWalletMetadata({});
+    setConnectionData(prev => ({ ...prev, pairedWalletData: undefined, pairedAccounts: [] }));
+  };
+
+  useEffect(() => {
+    initHashConnect();
+  }, []);
+
+  useEffect(() => {
+    hashconnectInstance &&
+      hashconnectInstance.foundExtensionEvent.once(walletMetadata => {
+        setWalletMetadata(walletMetadata);
+        setReadyToConnect(true);
+        setIsConnectionLoading(false);
+      });
+  }, [hashconnectInstance]);
+
+  useEffect(() => {
+    if (connectionData.topic !== '' && hashconnectInstance) {
+      hashconnectInstance.pairingEvent.once(pairingData => {
+        const { accountIds: pairedAccounts, metadata: pairedWalletData } = pairingData;
+        const objectToSave = { ...connectionData, pairedAccounts, pairedWalletData };
+
+        localStorage.setItem('hashconnectData', JSON.stringify(objectToSave));
+
+        setConnected(true);
+        setIsConnectionLoading(false);
+      });
+    }
+  }, [hashconnectInstance, connectionData]);
+
   return (
     <BrowserRouter>
       <div className="wrapper">
-        <Header />
+        <Header
+          connected={connected}
+          connectWallet={connectWallet}
+          disconnectWallet={disconnectWallet}
+          readyToConnect={readyToConnect}
+          isConnectionLoading={isConnectionLoading}
+        />
         <div className="main">
           <div className="container py-5 py-lg-7">
             <Routes>
--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -2,21 +2,48 @@ import React from 'react';
 
 interface IHeaderProps {
   connected: boolean;
-  setConnected: React.Dispatch<React.SetStateAction<boolean>>;
+  isConnectionLoading: boolean;
+  readyToConnect: boolean;
+  connectWallet: () => void;
+  disconnectWallet: () => void;
 }
 
-const Header = ({ connected, setConnected }: IHeaderProps) => {
+const Header = ({
+  connected,
+  connectWallet,
+  disconnectWallet,
+  readyToConnect,
+  isConnectionLoading,
+}: IHeaderProps) => {
   return (
     <div className="container py-3">
       <div className="d-flex justify-content-end">
         <div className="d-flex align-items-center">
-          {connected ? (
-            <>
-              <p className="text-success mx-2">Connected</p>
-              <button className="btn btn-sm btn-outline-primary mx-2">Disconnect</button>
-            </>
+          {!isConnectionLoading ? (
+            readyToConnect ? (
+              connected ? (
+                <>
+                  <p className="text-success mx-2">Connected</p>
+                  <button
+                    onClick={() => disconnectWallet()}
+                    className="btn btn-sm btn-outline-primary mx-2"
+                  >
+                    Disconnect
+                  </button>
+                </>
+              ) : (
+                <button
+                  onClick={() => connectWallet()}
+                  className="btn btn-sm btn-outline-primary mx-2"
+                >
+                  Connect to wallet
+                </button>
+              )
+            ) : (
+              <p className="text-warning mx-2">Please install a wallet</p>
+            )
           ) : (
-            <button className="btn btn-sm btn-outline-primary mx-2">Connect to wallet</button>
+            <p className="text-success mx-2">Loading...</p>
           )}
 
           <button className="btn btn-sm btn-primary mx-2">Bridging</button>
