f1bf86d2b755948e93aa8e27b16bcb788f7dbabb
Martin Dobrev <martindobrev87@gmail.com>
2022-06-23 12:53:21 +0300

    Icons updated

--- src/components/IconToken.tsx
+++ src/components/IconToken.tsx
@@ -3,6 +3,12 @@ import HBAR from '../icons/tokens/HBAR.svg';
 import ETH from '../icons/tokens/ETH.svg';
 
 import HELI from '../icons/tokens/HELI.png';
+import DOB from '../icons/tokens/DOB.png';
+import EIGHT from '../icons/tokens/EIGHT.png';
+import HEX from '../icons/tokens/HEX.webp';
+import USDT from '../icons/tokens/USDT.png';
+import WBTC from '../icons/tokens/WBTC.png';
+import WHBAR from '../icons/tokens/WHBAR.png';
 
 interface IIconTokenProps {
   symbol: string;
@@ -17,6 +23,13 @@ const tokenMapping: ITokenMapping = {
   HBAR: HBAR,
   ETH: ETH,
   HELI: HELI,
+  DOB: DOB,
+  EIGHT: EIGHT,
+  HEX: HEX,
+  USDT: USDT,
+  WBTC: WBTC,
+  WETH: ETH,
+  WHBAR: WHBAR,
 };
 
 const IconToken = ({ symbol, className }: IIconTokenProps) => {
