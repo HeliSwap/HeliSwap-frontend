c5071db1cb9a7f8afb4d5e47c1919df058a49be1
Martin Dobrev <martindobrev87@gmail.com>
2022-07-07 10:20:39 +0300

    Remove liquidity updated

--- src/components/PageHeader.tsx
+++ src/components/PageHeader.tsx
@@ -1,13 +1,21 @@
 import React from 'react';
+import Icon from './Icon';
 import Settings from './Settings';
 
 interface IPageHeaderProps {
   title: string;
+  handleBackClick?: () => void;
 }
 
-const PageHeader = ({ title }: IPageHeaderProps) => {
+const PageHeader = ({ title, handleBackClick }: IPageHeaderProps) => {
   return (
     <div className="d-flex justify-content-between align-items-center mb-6">
+      {handleBackClick ? (
+        <span className="cursor-pointer" onClick={handleBackClick}>
+          <Icon name="arrow-left" />
+        </span>
+      ) : null}
+
       <h1 className="text-subheader text-light">{title}</h1>
       <Settings />
     </div>
