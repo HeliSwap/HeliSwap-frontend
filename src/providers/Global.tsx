d3313c91b9642a0920622654c477800ddbcfffc5
Martin Dobrev <martindobrev87@gmail.com>
2022-07-15 10:20:14 +0300

    Health check updated

--- src/providers/Global.tsx
+++ src/providers/Global.tsx
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from 'react';
 import Hashconnect from '../connectors/hashconnect';
+import { HEALTH_CHECK_INTERVAL } from '../constants';
 import useHealthCheck from '../hooks/useHealthCheck';
 import SDK from '../sdk/sdk';
 
@@ -35,11 +36,15 @@ export const GlobalProvider = ({ children }: IGlobalProps) => {
 
   const { timestamp, error } = useHealthCheck({
     fetchPolicy: 'network-only',
-    pollInterval: 5000,
+    pollInterval: HEALTH_CHECK_INTERVAL,
   });
 
   const nowSeconds = Date.now() / 1000;
-  const isRunning = !error ? (timestamp ? nowSeconds >= Number(timestamp) : false) : false;
+  const isRunning = !error
+    ? timestamp
+      ? nowSeconds + HEALTH_CHECK_INTERVAL / 1000 >= Number(timestamp)
+      : false
+    : false;
 
   const connectWallet = () => {
     hashconnectConnectorInstance?.connect();
