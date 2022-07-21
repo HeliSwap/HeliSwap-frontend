64fc3c5a56043b61458b19cdd1dce0f7868984a5
Martin Dobrev <martindobrev87@gmail.com>
2022-07-21 14:15:09 +0300

    Close modal on escape click

--- src/components/Modal.tsx
+++ src/components/Modal.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect, useState, useCallback } from 'react';
 
 interface IModalProps {
   show?: boolean;
@@ -6,6 +6,32 @@ interface IModalProps {
 }
 
 const Modal = ({ show = true, children }: IModalProps) => {
+  const [toShow, setToShow] = useState(show);
+
+  const handleKeyDown = useCallback((event: any) => {
+    if (event.key === 'Escape') {
+      setToShow(prev => prev && false);
+    }
+  }, []);
+
+  useEffect(() => {
+    setToShow(show);
+
+    return () => {
+      setToShow(false);
+    };
+  }, [show]);
+
+  useEffect(() => {
+    document.addEventListener('keydown', handleKeyDown);
+
+    return () => {
+      document.removeEventListener('keydown', handleKeyDown);
+    };
+  }, [handleKeyDown]);
+
+  console.log('toShow', toShow);
+
   return (
     <>
       <div
@@ -13,13 +39,16 @@ const Modal = ({ show = true, children }: IModalProps) => {
         id="exampleModal"
         aria-labelledby="exampleModalLabel"
         aria-hidden="true"
-        style={{ display: show ? 'block' : 'none' }}
+        style={{ display: toShow ? 'block' : 'none' }}
       >
         <div className="modal-dialog modal-dialog-centered">
           <div className="modal-content">{children}</div>
         </div>
       </div>
-      <div style={{ display: show ? 'block' : 'none' }} className="modal-backdrop fade show"></div>
+      <div
+        style={{ display: toShow ? 'block' : 'none' }}
+        className="modal-backdrop fade show"
+      ></div>
     </>
   );
 };
