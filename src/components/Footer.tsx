c2fa269dd951053fe2ee59298529f8ff146bd005
Martin Dobrev <martindobrev87@gmail.com>
2022-07-26 13:01:54 +0300

    Tippy styles updated, Sidebar updated

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,4 +1,5 @@
 import React, { useContext } from 'react';
+import Tippy from '@tippyjs/react';
 import { GlobalContext } from '../providers/Global';
 
 const Footer = () => {
@@ -7,10 +8,12 @@ const Footer = () => {
 
   return (
     <div className="d-flex justify-content-end pe-4 mb-4">
-      <div className="d-flex align-items-center">
-        <span className="me-3 text-micro">{lastUpdated}</span>
-        <span className={`icon-healthcheck ${isRunning ? 'is-running' : ''}`}></span>
-      </div>
+      <Tippy content="The time at which the application was updated. Prices are updated up to this timestamp.">
+        <div className="d-flex align-items-center">
+          <span className="me-3 text-micro">{lastUpdated}</span>
+          <span className={`icon-healthcheck ${isRunning ? 'is-running' : ''}`}></span>
+        </div>
+      </Tippy>
     </div>
   );
 };
