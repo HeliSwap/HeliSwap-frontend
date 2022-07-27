8f84f2b1596b6b72d5702df04279001e6bad8f04
Dimitar Atanasov <dimitar.atanasov@limechain.tech>
2022-07-27 17:35:04 +0300

    Search for extra pools initial implementation

---
+++ src/components/SearchArea.tsx
@@ -0,0 +1,63 @@
+import React, { useEffect, useState, useMemo, useCallback } from 'react';
+// import debounce from 'lodash.debounce';
+const debounce = require('lodash.debounce');
+
+interface ISearchAreaProps {
+  searchFunc: (value: string) => void;
+  calledSearchResults: boolean;
+  loadingSearchResults: boolean;
+  results: Object[];
+}
+
+const SearchArea = ({
+  searchFunc,
+  calledSearchResults,
+  loadingSearchResults,
+  results,
+}: ISearchAreaProps) => {
+  const [inputValue, setInputValue] = useState('');
+
+  const handleSearch = useCallback(
+    (value: string) => {
+      if (value.length > 1) {
+        console.log('now searching for', value);
+        searchFunc(value);
+      }
+    },
+    [searchFunc],
+  );
+
+  const debouncedSearchHandler = useMemo(() => debounce(handleSearch, 1000), [handleSearch]);
+
+  useEffect(() => {
+    return () => {
+      debouncedSearchHandler.cancel();
+    };
+  }, [debouncedSearchHandler]);
+
+  return (
+    <div className="search-area-container">
+      <div className="search-area-wrapper">
+        <input
+          value={inputValue}
+          onChange={e => {
+            setInputValue(e.target.value);
+            debouncedSearchHandler(e.target.value);
+          }}
+          className="search-area-input"
+        ></input>
+      </div>
+      {calledSearchResults && loadingSearchResults ? (
+        <div>Loading...</div>
+      ) : (
+        <ul>
+          {(results || []).map((item: any, index: number) => {
+            return <li key={index}>{item.pairName}</li>;
+          })}
+        </ul>
+      )}
+    </div>
+  );
+};
+
+export default SearchArea;
