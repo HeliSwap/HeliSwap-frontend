2899cb197fa7d6b25965acabf562a426365e07a6
Martin Dobrev <martindobrev87@gmail.com>
2022-08-15 11:42:18 +0300

    Messages updated

--- src/content/messages.tsx
+++ src/content/messages.tsx
@@ -31,18 +31,24 @@ export const tokenPropsMessages: IStringToHTMLElement = {
   ),
   pauseKey: (
     <span>
-      <span className="text-bold">Pause Key</span> -
+      <span className="text-bold">Pause Key</span> - The key which can sign to freeze or unfreeze an
+      account for token transactions. If empty, freezing is not possible.
     </span>
   ),
   supplyKey: (
     <span>
-      <span className="text-bold">Supply Key</span> -
+      <span className="text-bold">Supply Key</span> - A Supply key is set and the supply operator
+      has special privileges that may result in Manipulating the Pair reserves, thus changing the
+      exchange rate and disrupting the Pool. Liquidity providers may suffer from great impermanent
+      loss.
     </span>
   ),
   wipeKey: (
     <span>
       <span className="text-bold">Wipe Key</span> - The key which can wipe the token balance of an
-      account. If empty, wipe is not possible.
+      account. If empty, wipe is not possible Problem Wipe key can perform TokenWipe transactions.
+      If a user has balance of HTS token and TokenWipe is executed, his token balance will be burned
+      by the amount specified in the wipe operation without requesting permissions from the user.
     </span>
   ),
 };
