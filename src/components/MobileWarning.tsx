82bc5a0371a84af51af0d0df7f86090c5561f37b
Martin Dobrev <martindobrev87@gmail.com>
2022-10-11 11:32:30 +0300

    Mobile warning updated

--- src/components/MobileWarning.tsx
+++ src/components/MobileWarning.tsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 
+import Icon from './Icon';
+
 const MobileWarning = () => {
   return (
     <div className="py-4">
@@ -8,10 +10,20 @@ const MobileWarning = () => {
         <Link to="/">
           <img src="/logo-full.svg" alt="" />
         </Link>
-        <h1 className="text-subheader text-bold mt-5">Mobile version coming soon…</h1>
-        <p className="alert alert-warning text-main text-warning mt-4 p-3">
-          Please connect from desktop computer.
-        </p>
+        <div className="text-center">
+          <img className="img-fluid" src="/mobile-device.svg" alt="" />
+          <h1 className="text-subheader text-bold mt-5">Mobile version coming soon…</h1>
+          <p className="text-main mt-4 p-3">Please connect from desktop computer.</p>
+          <div className="mt-5">
+            <a
+              className="link-primary text-bold d-inline-flex align-items-center"
+              href="https://heliswap.io"
+            >
+              <Icon color="info" name="arrow-left" />
+              <span className="ms-2">Go to Landing page</span>
+            </a>
+          </div>
+        </div>
       </div>
     </div>
   );
