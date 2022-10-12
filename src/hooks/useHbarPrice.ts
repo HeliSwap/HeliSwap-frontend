fded53f012114ed60b3b29b96631189bf966ff6b
Martin Dobrev <martindobrev87@gmail.com>
2022-10-12 14:32:09 +0300

    Coingecko removed as price fetching dependency

--- src/hooks/useHbarPrice.ts
+++ src/hooks/useHbarPrice.ts
@@ -6,18 +6,32 @@ const useHbarPrice = () => {
   const [hbarPrice, setHbarPrice] = useState<number>(0);
   const [loading, setLoading] = useState(false);
 
+  const getPriceFromBase64 = (encoded: any) => {
+    const priceString = atob(encoded);
+    const splittedOnce = priceString.split(',');
+    const splittedTwice = splittedOnce[0].split(':');
+
+    return Number(splittedTwice[1].trim());
+  };
+
   const getHBARPrice = useCallback(async () => {
     setLoading(true);
-    const coingeckoURL = process.env.REACT_APP_COINGECKO_URL + `/simple/price`;
-    try {
-      const response = await axios.get(coingeckoURL, {
-        params: {
-          ids: 'hedera-hashgraph',
-          vs_currencies: 'usd',
-        },
-      });
+    const currentTimestampInSeconds = Date.now() / 1000;
+    const accountID = '0.0.57';
 
-      setHbarPrice(response.status === 200 ? response.data['hedera-hashgraph']['usd'] : hbarPrice);
+    const url = `${process.env.REACT_APP_MIRROR_NODE_URL}/api/v1/transactions?account.id=${accountID}&transactiontype=fileupdate&limit=1&timestamp=lt:${currentTimestampInSeconds}`;
+
+    try {
+      const response = await axios.get(url);
+      const {
+        data: { transactions },
+        status,
+      } = response;
+
+      const { memo_base64 } = transactions[0];
+      const currentRate = getPriceFromBase64(memo_base64);
+
+      setHbarPrice(status === 200 ? currentRate : hbarPrice);
     } catch (e) {
       console.error(e);
       setHbarPrice(hbarPrice);
