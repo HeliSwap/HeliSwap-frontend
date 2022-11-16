729760163aa6cfebb6c77ddb5226cc7b14d45c9a
Dimitar Atanasov <dimitar.atanasov@limechain.tech>
2022-11-16 17:43:55 +0200

    Remove refetching

--- src/hooks/useHistoricalData.ts
+++ src/hooks/useHistoricalData.ts
@@ -2,22 +2,14 @@ import { useEffect, useState } from 'react';
 
 import { QueryHookOptions, useQuery } from '@apollo/client';
 import { GET_METRICS } from '../GraphQL/Queries';
-import { REFRESH_TIME } from '../constants';
 
 import { IHistoricalData } from '../interfaces/common';
 
 const useHistoricalData = (useQueryOptions: QueryHookOptions = {}) => {
   const [historicalData, setHistoricalData] = useState<IHistoricalData[]>([]);
-  const { loading, data, error, startPolling, stopPolling, refetch } = useQuery(GET_METRICS, {
+  const { loading, data, error, refetch } = useQuery(GET_METRICS, {
     ...useQueryOptions,
   });
-
-  useEffect(() => {
-    startPolling(useQueryOptions.pollInterval || REFRESH_TIME);
-    return () => {
-      stopPolling();
-    };
-  }, [startPolling, stopPolling, useQueryOptions]);
 
   useEffect(() => {
     if (data) {
