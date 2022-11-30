158e4fc13395138437f0d0ee6056016417dd2376
Dimitar Atanasov <dimitar.atanasov@limechain.tech>
2022-11-30 14:23:47 +0200

    Separate maintain reward details in component

--- src/pages/MaintainFarms.tsx
+++ src/pages/MaintainFarms.tsx
@@ -1,16 +1,20 @@
 import React, { useContext, useEffect, useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 
-import Button from '../components/Button';
-import toast from 'react-hot-toast';
-import FarmsSDK from '../sdk/farmsSdk';
-import ToasterWrapper from '../components/ToasterWrapper';
-import useFarms from '../hooks/useFarms';
-import { useQueryOptionsPoolsFarms } from '../constants';
-import usePoolsByTokensList from '../hooks/usePoolsByTokensList';
 import { GlobalContext } from '../providers/Global';
+
+import FarmsSDK from '../sdk/farmsSdk';
+
+import ToasterWrapper from '../components/ToasterWrapper';
+
 import Loader from '../components/Loader';
 import FarmRow from '../components/FarmRow';
+import DeployFarm from '../components/DeployFarm';
+
+import useFarms from '../hooks/useFarms';
+import usePoolsByTokensList from '../hooks/usePoolsByTokensList';
+
+import { useQueryOptionsPoolsFarms } from '../constants';
 
 const MaintainFarms = () => {
   // Context values
@@ -102,50 +106,6 @@ const MaintainFarms = () => {
         )}
         <ToasterWrapper />
       </div>
-    </div>
-  );
-};
-
-interface IDeployFarmProps {
-  farmsSDK: FarmsSDK;
-}
-
-const DeployFarm = ({ farmsSDK }: IDeployFarmProps) => {
-  const [tokenAddress, setTokenAddress] = useState<string>('');
-  const [newFarmAddress, setNewFarmAddress] = useState<string>('');
-  const [loadingDeploy, setLoadingDeploy] = useState<boolean>(false);
-
-  const deployFarm = async () => {
-    setLoadingDeploy(true);
-    try {
-      const farmAddress = await farmsSDK.deployFarm(tokenAddress);
-
-      setNewFarmAddress(farmAddress as string);
-      setTokenAddress('');
-      toast.success('Success! New farm was deployed.');
-    } catch (error) {
-      console.log(error);
-      toast.error('Error while deploying farm');
-    } finally {
-      setLoadingDeploy(false);
-    }
-  };
-
-  return (
-    <div className="m-4">
-      <h2 className={`text-main mb-3`}>Deploy new farm</h2>
-      <div className="d-flex align-items-center">
-        <input
-          className="form-control"
-          value={tokenAddress}
-          placeholder="Staking token address"
-          onChange={(e: any) => setTokenAddress(e.target.value)}
-        />
-        <Button className="ms-3" onClick={deployFarm} loading={loadingDeploy}>
-          Deploy
-        </Button>
-      </div>
-      {newFarmAddress ? <div>{`New farm deployed at address: ${newFarmAddress}`}</div> : null}
     </div>
   );
 };
