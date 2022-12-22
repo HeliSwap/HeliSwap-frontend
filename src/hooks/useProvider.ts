39d7042f9babbeb2f1a8b163969758eaefdee7ad
Martin Dobrev <martindobrev87@gmail.com>
2022-12-22 13:11:28 +0200

    Global provider updated, hook removed

--- src/hooks/useProvider.ts
+++
@@ -1,9 +0,0 @@
-import { ethers } from 'ethers';
-
-const useProvider = () => {
-  const provider = new ethers.providers.JsonRpcProvider(process.env.REACT_APP_PROVIDER_URL);
-
-  return provider;
-};
-
-export default useProvider;
