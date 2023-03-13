d5b214e8bdc1934d1475ea97a1ccdd5f140d5da1
Martin Dobrev <martindobrev87@gmail.com>
2023-03-13 17:11:30 +0200

    Blade connector updated

--- src/connectors/blade.ts
+++ src/connectors/blade.ts
@@ -1,4 +1,6 @@
+import { AccountId, Transaction, TransactionId } from '@hashgraph/sdk';
 import { BladeSigner, HederaNetwork } from '@bladelabs/blade-web3.js';
+import { randomIntFromInterval } from '../utils/numberUtils';
 
 class BladeConnect {
   signer: BladeSigner;
@@ -26,6 +28,29 @@ class BladeConnect {
     this.signer = bladeSigner;
     this.setConnected(true);
     this.setUserId(bladeSigner.getAccountId().toString());
+  }
+
+  async sendTransaction(transaction: Transaction) {
+    return await this.signer.call(transaction);
+  }
+
+  async freezeTransaction(trans: Transaction, userId: string) {
+    let transId = TransactionId.generate(userId);
+    trans.setTransactionId(transId);
+
+    let nodeId = 5;
+    //Choose random node depending on the network selected (we exclude some of the nodes as the current hashgraph/sdk version 2.16.2 used by hashconnect doesn't support all of the available hedera nodes)
+    if (process.env.REACT_APP_NETWORK_TYPE === 'testnet') {
+      nodeId = randomIntFromInterval(3, 7);
+    } else if (process.env.REACT_APP_NETWORK_TYPE === 'mainnet') {
+      nodeId = randomIntFromInterval(5, 20);
+    }
+
+    trans.setNodeAccountIds([new AccountId(nodeId)]);
+
+    trans.freeze();
+
+    return trans;
   }
 }
 
