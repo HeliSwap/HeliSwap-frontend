e2fc360a3a212d407d3f39a4bf2efd1f4db61e18
Martin Dobrev <martindobrev87@gmail.com>
2023-10-04 08:12:24 +0300

    Boosted pool added

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -158,7 +158,10 @@ export const restrictedFarms = [
 
 export const bladeClaimdropAddress = '0x00000000000000000000000000000000002fe4bd';
 
-export const boostedPools = ['0x5718F1127E025C6745835faCF551283481630423'];
+export const boostedPools = [
+  '0x5718F1127E025C6745835faCF551283481630423',
+  '0xEc9396Fd1019a6644534Ce7905183F495bC86baf',
+];
 
 export const notVerifiedTokens = [
   '0x00000000000000000000000000000000002A30A8',
