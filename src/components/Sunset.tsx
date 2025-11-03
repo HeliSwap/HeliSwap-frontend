6fd8e7ebadd91eb5f74b93af6b6726e1ea7b7106
Martin Dobrev <martindobrev87@gmail.com>
2025-11-03 10:46:45 +0200

    feat: sunset screen

---
+++ src/components/Sunset.tsx
@@ -0,0 +1,18 @@
+const Sunset = () => {
+  return (
+    <div className="container py-5 py-lg-20">
+      <h1 className="text-display text-center">HeliSwap has terminated its operations</h1>
+      <p className="text-body text-center mt-4">
+        We sincerely appreciate the support, trust, and participation of everyone who has been part
+        of this journey. Thank you for being with us, and we wish you the very best moving forward.
+        If you have any liquidity in the pools or farms, please open a ticket in our support{' '}
+        <a href="https://discord.com/invite/wVrkMwBKsm" target="_blank" rel="noopener noreferrer">
+          channel
+        </a>
+        .
+      </p>
+    </div>
+  );
+};
+
+export default Sunset;
